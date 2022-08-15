17f388619088c6bea324b18df3f569a0bb29b866
igorfelipeduca <duccaigor@protonmail.com>
2022-08-15 01:09:57 -0300

    Titulo do header menor para mobile

--- components/layout/checkoutHeader/index.tsx
+++ components/layout/checkoutHeader/index.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  Heading,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 import { FiHome } from "react-icons/fi";
 import Zdn from "../../../assets/zdn.png";
 
@@ -15,7 +9,35 @@ type checkoutHeaderProps = {
   itemAmount: number;
 };
 
+const useMediaQuery = (width: number) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: any) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, [updateTarget, width]);
+
+  return targetReached;
+};
+
 const CheckoutHeader: React.FC<checkoutHeaderProps> = ({ itemAmount }) => {
+  const isBreakpoint = useMediaQuery(768);
+
   return (
     <>
       <Box
@@ -41,11 +63,11 @@ const CheckoutHeader: React.FC<checkoutHeaderProps> = ({ itemAmount }) => {
 
         <Box>
           {itemAmount > 1 ? (
-            <Text fontSize={"2xl"} fontWeight={"bold"}>
+            <Text fontSize={isBreakpoint ? "xl" : "2xl"} fontWeight={"bold"}>
               Finalizar a compra ({itemAmount} itens)
             </Text>
           ) : (
-            <Text fontSize={"2xl"} fontWeight={"bold"}>
+            <Text fontSize={isBreakpoint ? "xl" : "2xl"} fontWeight={"bold"}>
               Finalizar a compra ({itemAmount} item)
             </Text>
           )}
